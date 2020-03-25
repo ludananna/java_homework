59efe5f4eb8b9762ce6ecd6bd05e5a0c2d037870
anna <ludan.anna@mail.ru>
2020-03-25 13:51:32 +0100

    Added solution for Prime Numbers task

---
+++ src/more/PrimeNumbers.java
@@ -0,0 +1,21 @@
+package more;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        for (int x = 6; x <= 100; x++) {
+            boolean isPrime = true;
+            int y = 2;
+            while (y <= x / 2) {
+                if (x % y == 0) {
+                    isPrime = false;
+                    break;
+                }
+                y++;
+            }
+            if (isPrime)
+                System.out.print(x + " ");
+        }
+    }
+}
+

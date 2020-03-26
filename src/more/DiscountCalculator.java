1d60430c4392579c78d326123621c67849086e49
anna <ludan.anna@mail.ru>
2020-03-26 20:51:51 +0100

    Added task about discount calculator

--- src/more/DiscountCalculator.java
+++ src/more/DiscountCalculator.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class DiscountCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println(" Please, provide item price:");
+        System.out.print("Please, provide item price:");
         int price = scan.nextInt();
-        System.out.println(" Please, input customer age:");
+        System.out.print("Please, input customer age:");
         int age = scan.nextInt();
+        double discount = 1;
         if (age < 6) {
-            System.out.format("Discounted price is " + (price * 0.2));
-        } else if (age < 18 && age > 6) {
-            System.out.format("Discounted price is " + (price * 0.75));
+            discount = 0.2;
+        } else if (age <= 18) {
+            discount = 0.75;
         } else if (age > 65) {
-            System.out.format("Discounted price is " + (price * 0.7));
+            discount = 0.7;
         }
+        System.out.format("Discounted price is " + (price * discount));
     }
 }

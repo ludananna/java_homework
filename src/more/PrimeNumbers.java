c9508aae87c5debec92ee4680bbd01ddbd67bfbf
anna <ludan.anna@mail.ru>
2020-03-26 18:23:34 +0100

    Added first prime numbers in row

--- src/more/PrimeNumbers.java
+++ src/more/PrimeNumbers.java
@@ -3,7 +3,7 @@ package more;
 public class PrimeNumbers {
 
     public static void main(String[] args) {
-        for (int x = 6; x <= 100; x++) {
+        for (int x = 1; x <= 100; x++) {
             boolean isPrime = true;
             int y = 2;
             while (y <= x / 2) {

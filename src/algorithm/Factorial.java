c945b3bd8def904a64b7698a810e92aeb416b753
anna <ludan.anna@mail.ru>
2020-04-16 20:31:45 +0200

    Factorial task fixed

--- src/algorithm/Factorial.java
+++ src/algorithm/Factorial.java
@@ -4,12 +4,9 @@ public class Factorial {
 
 
     private static int factorial(int n) {
-
         if (n == 0) {
+            System.out.print("1");
             return 1;
-        }if (n < 0) {
-
-            return Integer.parseInt(null);
         }
         int i = n * factorial(n - 1);
         System.out.print(" " + i);

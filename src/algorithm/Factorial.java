5a16542e81115683b37716ae0ad793d9927d98de
anna <ludan.anna@mail.ru>
2020-04-16 20:16:14 +0200

    Factorial task fixed

--- src/algorithm/Factorial.java
+++ src/algorithm/Factorial.java
@@ -1,26 +1,22 @@
 package algorithm;
 
 public class Factorial {
-    public static void main(String[] args) {
-        System.out.print(factorial(1) + " ");
-        System.out.print(factorial(2)+ " ");
-        System.out.print(factorial(3)+ " ");
-        System.out.print(factorial(4)+ " ");
-        System.out.print(factorial(5)+ " ");
-        System.out.print(factorial(6)+ " ");
-        System.out.print(factorial(7)+ " ");
-        System.out.print(factorial(8)+ " ");
-        System.out.print(factorial(9)+ " ");
-        System.out.print(factorial(10)+ " ");
 
+
+    private static int factorial(int n) {
+
+        if (n == 0) {
+            return 1;
+        }if (n < 0) {
+
+            return Integer.parseInt(null);
+        }
+        int i = n * factorial(n - 1);
+        System.out.print(" " + i);
+        return i;
     }
 
-    public static int factorial(int n) {
-        if (n == 1) {
-            return 1;
-        } else {
-
-            return n * factorial(n - 1);
-        }
+    public static void main(String[] args) {
+       factorial(10);
     }
 }

f986522015738a0739c2389064101e490192f053
anna <ludan.anna@mail.ru>
2020-04-19 10:43:55 +0200

    Task words fixed

--- src/algorithm/Words.java
+++ src/algorithm/Words.java
@@ -6,28 +6,31 @@ public class Words {
     public static void main(String[] args) {
         String words = "For given text find the longest and the shortest words";
 
-        String[] split = words.split("\\s+");
-        String max = split[0];
-        String min = split[0];
+        String[] sentence = words.split("\\s+");
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
         String minWords = "";
         String maxWords = "";
-        System.out.println("split = " + Arrays.toString(split));
-        for (String s : split) {
-            if (max.length() < s.length()) {
-                max = s;
-                maxWords += max + ", ";
+        System.out.println("sentence = " + Arrays.toString(sentence));
+        for (String word : sentence) {
+            int wordLength = word.length();
+            if (maxValue < wordLength) {
+                maxValue = wordLength;
+                maxWords = "";
             }
-            if (min.length() >= s.length()) {
-                min = s;
-                minWords += min + ", ";
-
+            if (maxValue == wordLength) {
+                maxWords = maxWords + word + ", ";
+            }
+            if (minValue > wordLength) {
+                minValue = wordLength;
+                minWords = "";
+            }
+            if (minValue == wordLength) {
+                minWords = minWords + word + ", ";
             }
         }
-        System.out.println("max = " + max);
-        System.out.println("min = " + min);
         System.out.println("minWords = " + minWords);
         System.out.println("maxWords = " + maxWords);
-
     }
 
 }

bdc26868cd1d287c38f7e56f585b3a0c93a672ae
anna <ludan.anna@mail.ru>
2020-04-30 21:31:17 +0200

    Task LetterCounter completed

---
+++ src/algorithm/LetterCounter.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+import java.util.*;
+
+public class LetterCounter {
+    public static void main(String[] args) {
+
+        String sentence = "This is a test text";
+        System.out.println("sentence = " + sentence);
+
+        char[] chars = sentence.toCharArray();
+
+        Map<Character, Integer> sentenceMap = new HashMap<>();
+        for (char aChar : chars) {
+
+            if (sentenceMap.containsKey(aChar)) {
+                Integer integer = sentenceMap.get(aChar);
+                sentenceMap.put(aChar, integer + 1);
+            } else {
+                sentenceMap.put(aChar, 1);
+            }
+        }
+        System.out.println("sentenceMap = " + sentenceMap);
+    }
+}
+

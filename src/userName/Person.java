2ead205d078a39d71294ffaa80c2aa5796901771
anna <ludan.anna@mail.ru>
2020-05-26 19:04:02 +0200

    Task  Person  completed

---
+++ src/userName/Person.java
@@ -0,0 +1,68 @@
+package userName;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDay;
+
+    public Person(String firstName, String lastName, LocalDate birthDay) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+    }
+
+
+    public String getUserName() {
+        return firstName.toLowerCase().toCharArray()[0]
+                + lastName.toLowerCase()
+                + Optional.ofNullable(birthDay).map(LocalDate::getYear).orElseGet(() -> LocalDate.now().getYear());
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (getFirstName() != null ? !getFirstName().equals(person.getFirstName()) : person.getFirstName() != null)
+            return false;
+        if (getLastName() != null ? !getLastName().equals(person.getLastName()) : person.getLastName() != null)
+            return false;
+        return getBirthDay() != null ? getBirthDay().equals(person.getBirthDay()) : person.getBirthDay() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFirstName() != null ? getFirstName().hashCode() : 0;
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = 31 * result + (getBirthDay() != null ? getBirthDay().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDay=" + birthDay +
+                '}';
+    }
+}
---
+++ src/userName/PersonsTester.java
@@ -0,0 +1,17 @@
+package userName;
+
+import java.time.LocalDate;
+
+public class PersonsTester {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Anna", "Kogut", LocalDate.of(2000, 3, 21));
+        Person p2 = new Person("Hanna", "Wukanowich", LocalDate.of(1952, 6, 11));
+        Person p3 = new Person("Zena", "Queen", null);
+
+        System.out.println("p1.getUserName() = " + p1.getUserName());
+        System.out.println("p2.getUserName() = " + p2.getUserName());
+        System.out.println("p3.getUserName() = " + p3.getUserName());
+
+    }
+}

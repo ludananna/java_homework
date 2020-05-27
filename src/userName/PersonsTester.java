2eb2d4f8e5c5c580d4e38eb1a22f35cd03ba103f
anna <ludan.anna@mail.ru>
2020-05-27 20:28:27 +0200

    Task  UserName  fixed

--- src/userName/PersonsTester.java
+++ src/userName/PersonsTester.java
@@ -9,6 +9,7 @@ public class PersonsTester {
         Person p2 = new Person("Hanna", "Wukanowich", LocalDate.of(1952, 6, 11));
         Person p3 = new Person("Zena", "Queen", null);
 
+
         System.out.println("p1.getUserName() = " + p1.getUserName());
         System.out.println("p2.getUserName() = " + p2.getUserName());
         System.out.println("p3.getUserName() = " + p3.getUserName());
